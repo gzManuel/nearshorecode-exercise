32a1f701e92eeb11b2dc0c9e517557d15a79ca26
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-10-11 00:48:25 -0400

    refactor: refactor information test

--- backend/src/test/information.js
+++ backend/src/test/information.js
@@ -1,30 +1,44 @@
+
 const expect = require('chai').expect;
 const request = require('supertest');
 
 const app = require('../app.js');
+const conn = require('../utils/db.js')
 
-describe('POST /information',()=>{
-    it('OK, getting amount of the user',(done)=>{
-        request(app).post('/information')
-        .send({email:"user1@test.com"})
-        .then(res=>{
-            const body = res.body;
-            expect(body).to.contain.property('amount');
-            done();
-        }).catch((err)=>done(err));
+require('dotenv').config();
+
+describe('POST /information', () => {
+
+    before((done) => {
+        conn.connect()
+            .then(() => done())
+            .catch((err) => done(err));
     })
 
-});
-
-describe('POST /information',()=>{
-    it('OK, getting an empty {} json',(done)=>{
-        request(app).post('/information')
-        .send({email:"user1@fail.com"})
-        .then(res=>{
-            const body = res.body;
-            expect(Object.keys(body)).to.have.lengthOf(0);
-            done();
-        }).catch((err)=>done(err));
+    after((done) => {
+        conn.close()
+            .then(() => done())
+            .catch((err) => done(err));
     })
 
+    it('OK, getting amount of the user', (done) => {
+        request(app).post('/information')
+            .send({ email: "user1@test.com" })
+            .then(res => {
+                const body = res.body;
+                expect(body).to.contain.property('amount');
+                done();
+            }).catch((err) => done(err));
+    });
+
+    it('OK, getting an empty {} json', (done) => {
+        request(app).post('/information')
+            .send({ email: "user1@fail.com" })
+            .then(res => {
+                const body = res.body;
+                expect(Object.keys(body)).to.have.lengthOf(0);
+                done();
+            }).catch((err) => done(err));
+    });
+    
 });
