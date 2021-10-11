d705000c8482e210e6cc74131aebc55c5faacfe5
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-10-10 21:59:35 -0400

    fix: When running server didn't use the .env Files

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose');
 const mainRoutes = require('./routes/main.routes');
 const app = express();
 const cors = require('cors')
+require('dotenv').config();
+
 mongoose.connect(
-    'mongodb://127.0.0.1:27017/loans?directConnection=true&serverSelectionTimeoutMS=2000'
+    process.env.MONGO_URL
 );
 
 app.use(express.json());
